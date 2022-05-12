fb32fc4a4f184e56fa6f47d0e5d270592a730192
silvertechhub <agubatasylvester@gmail.com>
2022-05-12 12:02:44 +0100

    the ne ui

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+ content:['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        "body" :"#101010",
+        "hero": "#D62828",
+      },
+      backgroundImage: {
+        "hero-bg": "url('/assets/Backgroundsvg.svg')",
+        "bg-vh": "url('/assets/Background.png')"
+      }
+    },
+  },
+  plugins: [],
+}
